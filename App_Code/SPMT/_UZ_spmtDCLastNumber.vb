ec814cae9391ab84ba56f9d24eec39f9f35fb82b
0340LalitGupta <lalit@isgec.co.in>
2019-02-21 18:13:34 +0530

    Change of DC Series
    Change of DC Series

--- App_Code/SPMT/_UZ_spmtDCLastNumber.vb
+++ App_Code/SPMT/_UZ_spmtDCLastNumber.vb
@@ -5,6 +5,9 @@ Imports System.Data.SqlClient
 Imports System.ComponentModel
 Namespace SIS.SPMT
   Partial Public Class spmtDCLastNumber
+    Public Property JobWorkPrefix As String = ""
+    Public Property SupplyPrefix As String = ""
+    Public Property ERPVoucherSeries As String = ""
     Public Function GetColor() As System.Drawing.Color
       Dim mRet As System.Drawing.Color = Drawing.Color.Blue
       Return mRet
@@ -41,6 +44,41 @@ Namespace SIS.SPMT
         Return mRet
       End Get
     End Property
+    Public Shared Function GetNextDCNo(ByVal DCType As String, ByVal IssuerGSTIN As String) As String
+      Dim mRet As String = ""
+      Dim Results As SIS.SPMT.spmtDCLastNumber = Nothing
+      Using Con As SqlConnection = New SqlConnection(SIS.SYS.SQLDatabase.DBCommon.GetConnectionString())
+        Con.Open()
+        Using Cmd As SqlCommand = Con.CreateCommand()
+          Cmd.CommandType = CommandType.Text
+          Cmd.CommandText = "select top 1 * from spmt_dclastnumber where active = 1"
+          Dim Reader As SqlDataReader = Cmd.ExecuteReader()
+          If Reader.Read() Then
+            Results = New SIS.SPMT.spmtDCLastNumber(Reader)
+          End If
+          Reader.Close()
+        End Using
+      End Using
+      Select Case DCType
+        Case "J"
+          mRet = Results.JobWorkPrefix
+        Case "S"
+          mRet = Results.SupplyPrefix
+      End Select
+      mRet &= Results.ERPVoucherSeries
+      mRet &= IssuerGSTIN.Substring(0, 2)
+      Using Con As SqlConnection = New SqlConnection(SIS.SYS.SQLDatabase.DBCommon.GetConnectionString())
+        Con.Open()
+        Using Cmd As SqlCommand = Con.CreateCommand()
+          Cmd.CommandType = CommandType.Text
+          Cmd.CommandText = "select right(isnull(max(challanid),'0000000000'),5) FROM SPMT_DCHeader where left(challanid,5)='" & mRet & "'"
+          Dim tmp As String = Cmd.ExecuteScalar
+          mRet &= (Convert.ToInt32(tmp) + 1).ToString.PadLeft(5, "0")
+        End Using
+      End Using
+      Return mRet
+    End Function
+    'GetNextDCNo Discontinued on 12-02-2019
     Public Shared Function GetNextDCNo() As String
       Dim mRet As String = ""
       Dim Results As SIS.SPMT.spmtDCLastNumber = Nothing
